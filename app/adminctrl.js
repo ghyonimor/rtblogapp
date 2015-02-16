40cd54aa41d238e6dde0ef0376b573a1eaa20bd4
Yoni Mor <yonimor89@hotmail.co.il>
2015-02-16 16:50:19 +0200

    Added admin panel filter 'orderBy'.

--- app/adminctrl.js
+++ app/adminctrl.js
@@ -5,12 +5,14 @@
 
 var app = angular.module('BlogApp');
 
-app.controller('AdminCtrl', ['$scope', 'activeService', 'postsService',
-	function($scope, activeService, postsService) {
+app.controller('AdminCtrl', ['$scope', '$location', 'sanitizeService', 'activeService', 'postsService',
+	function($scope, $location, sanitizeService, activeService, postsService) {
 
 	activeService.data = 1;
 
 	var promise = postsService.getPosts;
+
+	$scope.sanitize = sanitizeService.getUrl;
 
 	/**
 	 * Get posts data.
@@ -22,4 +24,14 @@ app.controller('AdminCtrl', ['$scope', 'activeService', 'postsService',
 	$scope.$on('$destroy', function(){
         activeService.data = null;
     });
+
+    $scope.check = function(post) {
+    	var url = $scope.sanitize(post.title);
+    	console.log($location.url());
+    	console.log(url);
+    	$location.url('admin/edit/post/' + url);
+    };
+
+    $scope.predicate = 'title';
+    $scope.reverse = false;
 }]);
