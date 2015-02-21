c020d405e059eb2053de77cac6b96829f3874b99
Yoni Mor <yonimor89@hotmail.co.il>
2015-02-21 14:35:00 +0200

    edit enabled

--- public/app/editctrl.js
+++ public/app/editctrl.js
@@ -5,9 +5,29 @@
 
 var app = angular.module('BlogApp');
 
-app.controller('EditCtrl', ['$scope', 'activeService', 'saveService',
-	function($scope, activeService, saveService) {
-	activeService.data = 1;
+app.controller('EditCtrl', ['$scope', '$http', '$routeParams',
+	'postsService', 'sanitizeService', 'activeService', 'saveService',
+	function($scope, $http, $routeParams, postsService, sanitizeService, activeService, saveService) {
+
+	var param = $routeParams.param;
+	var sanitize = sanitizeService.getUrl;
+	var promise = postsService.getPosts;
+
+	promise.then(function(result) {
+		$scope.data = result.data;
+		$scope.posts = $scope.data.posts;
+
+		for (var i = 0; i < $scope.posts.length; i++) {
+			var post = $scope.posts[i];
+			if (sanitize(post.title) === param) {
+				$scope.title = post.title;
+				$scope.author = post.author;
+				$scope.tags = post.tags;
+				$scope.description = post.description;
+				$scope.markdown = post.mdPath;
+			}
+		}
+	});
 
 	$scope.isEditMode = function() {
 		return true;
@@ -16,4 +36,6 @@ app.controller('EditCtrl', ['$scope', 'activeService', 'saveService',
 	$scope.save = saveService.save;
 
 	$scope.del = saveService.del;
+
+	activeService.data = 1;
 }]);
